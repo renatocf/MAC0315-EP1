2da04a1b6a547c536557f87d63b9f97f2e173f33
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-06-06 19:22:11 -0300

    Adding first structures for the project

---
+++ Config.mk
@@ -0,0 +1,33 @@
+# Project setting
+PROJECT  := MAC0315-EP1
+VERSION  := 0.1
+
+# Program settings
+BIN      := Simplex
+# SBIN     := # Same as above, but for shell-only binaries.
+# LIBEXEC  := # Again, but for binaries runnable only by
+#             # other programs, not normal users.
+
+# IGNORED  := # Files within Make dirs and bins to be ignored.
+
+# ARLIB    := # Static/Shared libraries' names. If one is a
+# SHRLIB   := # lib, all source files will make the library.
+
+# Flags
+# ASFLAGS  := # Assembly Flags
+# CFLAGS   := # C Flags
+# CXXFLAGS := # C++ Flags
+# LDFLAGS  := # Linker flags
+
+# Documentation
+# LICENSE  := # File with a License (def: LICENSE)
+# NOTICE   := # File with a Notice of the License, to be used
+#             # in the beggining of any file (def: NOTICE).
+# DOXYFILE := # Config file for Doxygen (def: Doxyfile)
+
+# Package info
+MAINTEINER_NAME := Renato Cordeiro Ferreira
+MAINTEINER_MAIL := renato.cordeiro.ferreira@
+SYNOPSIS        := Network Simplex implementation for MAC0315
+DESCRIPTION     := C++ Implementation of MAC0315 first program exercise
+
